9e38ef8a5985953bdf415b7918c33e656448a988
Akshay <aksrahangdale@gmail.com>
2023-03-05 17:41:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+try {
+    mongoose.connect("mongodb+srv://aks123:123@assgn.bpeatqy.mongodb.net/?retryWrites=true&w=majority");
+    console.log("DB Connected");
+  } catch (err) {
+    console.log(err);
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/users', require('./routes/userRoutes'));
+
+
+
+app.listen(5000, () => {
+    console.log("Backend Server running at 5000");
+  });
+  
