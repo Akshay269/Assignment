99dafd38a9981e5ffa1fce5ab5bb8b542aa85064
Akshay <aksrahangdale@gmail.com>
2023-03-07 14:12:55 +0530

    fix:build done

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', require('./routes/userRoutes'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+  app.post('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 
 app.listen(5000, () => {
     console.log("Backend Server running at 5000");
