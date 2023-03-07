46c9f47cafa27f9231b9057d52bc0315e6d5ed59
Akshay <aksrahangdale@gmail.com>
2023-03-07 15:17:55 +0530

    uncmt

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 
   app.post('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
