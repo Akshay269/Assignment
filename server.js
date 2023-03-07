b3b299c24465d0bc1204f99e761012a9f8770129
Akshay <aksrahangdale@gmail.com>
2023-03-07 15:13:31 +0530

    fix:comrt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors=require("cors");
-const path = require('path');
+
 
 try {
     mongoose.connect("mongodb+srv://aks123:123@assgn.bpeatqy.mongodb.net/?retryWrites=true&w=majority");
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
 
   app.post('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
